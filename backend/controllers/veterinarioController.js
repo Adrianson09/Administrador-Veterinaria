c65f833588c89d7bc9e65e3be472710bbfaa4025
Adrian Guerrero <adrianson@outlook.com>
2023-08-23 14:54:14 -0600

    end olvide password

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -2,6 +2,7 @@ import Veterinario from '../models/Veterinario.js'
 import generarJWT from '../helpers/generarJWT.js';
 import generarId from '../helpers/generarId.js';
 import emailRegistro from '../helpers/emailRegistro.js';
+import emailOlvidePassword from '../helpers/emailOlvidePassword.js';
 
  const registrar = async (req, res) => {
      const {email, nombre } = req.body
@@ -98,6 +99,13 @@ const perfil = (req, res) => {
         try {
             existeVeterinario.token = generarId()
             await existeVeterinario.save();
+
+            // Enviar Email para restablecer password
+            emailOlvidePassword({
+                email,
+                nombre: existeVeterinario.nombre,
+                token: existeVeterinario.token
+            });
             res.json({msd: 'Hemos enviado un email con las instrucciones para restablecer la contraseña'})
         } catch (error) {
             console.log(error)
--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -51,7 +51,7 @@ const Registrar = () => {
 
     return (
       <>    
-       <div>
+            <div>
                 <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y administra tus {""} <span className="text-black">pacientes</span> </h1>
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">

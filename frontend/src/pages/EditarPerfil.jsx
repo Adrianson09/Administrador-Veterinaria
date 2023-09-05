c0e412b4fe56a6fe320ddedceed19cfdaa407fdf
Adrian Guerrero <adrianson@outlook.com>
2023-09-04 18:28:04 -0600

    add edit profile and change password

---
+++ frontend/src/pages/EditarPerfil.jsx
@@ -0,0 +1,14 @@
+import AdminNav from "../components/AdminNav"
+
+const EditarPerfil = () => {
+  return (
+    <>
+        <AdminNav/>
+        <h2 className="font-black text-3xl text-center mt-10">Editar Perfil</h2>
+        <p className=" text-xl mt-5 mb-10 text-center">Modifica tus {''}
+        <span className="text-indigo-600 font-bold" >Datos aquí</span></p>
+    </>
+  )
+}
+
+export default EditarPerfil
